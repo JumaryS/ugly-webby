d6520c36d2f4d62781312f05ac0d1e1ad5938558
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-02-11 10:47:00 -0500

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstItem = document.querySelector('h3')
+
+firstItem.style.backgroundColor= 'yellow'
+firstItem.style.fontSize='6em'
+firstItem.style.fontStyle='capital'
+
+
+
+const secondItem = document.querySelector('textarea')
+secondItem.style.fontSize='2em'
